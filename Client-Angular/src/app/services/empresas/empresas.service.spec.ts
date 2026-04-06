e8ab322e28cc04e906a0c221f2336b898eeb9b10
JOR3NTO <jorgeedcobo@gmail.com>
2026-04-06 16:22:09 -0500

    test(empresas): añadir pruebas unitarias para empresas

--- Client-Angular/src/app/services/empresas/empresas.service.spec.ts
+++ Client-Angular/src/app/services/empresas/empresas.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
-
 import { EmpresasService } from './empresas.service';
+import { EMPRESAS } from '../../data/empresas.interface';
 
 describe('EmpresasService', () => {
   let service: EmpresasService;
@@ -10,7 +10,20 @@ describe('EmpresasService', () => {
     service = TestBed.inject(EmpresasService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  describe('Creación del servicio', () => {
+
+    it('debería crearse correctamente', () => {
+      expect(service).toBeTruthy();
+    });
+
+    it('getAllEmpresas debería retornar un observable con las empresas', (done) => {
+      service.getAllEmpresas().subscribe(empresas => {
+        expect(empresas).toEqual(EMPRESAS);
+        expect(empresas.length).toBe(EMPRESAS.length);
+        done();
+      });
+    });
+
   });
+
 });
